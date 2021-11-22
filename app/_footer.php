1ec101c176296b3726961c38c2e3e64f93de12c1
Hani <hani.lebguiret1@gmail.com>
2021-11-22 16:43:15 +0100

    footer correc

--- app/_footer.php
+++ app/_footer.php
@@ -1,6 +1,5 @@
-<?php
-include "_bootstrap.php";
-?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
 <footer class="mt-5 p-3 bg-dark">
     <p class="text-center text-light">Copyright 2021</p>
